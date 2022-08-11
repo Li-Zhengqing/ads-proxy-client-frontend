249fbbe1ec23ff20372ca4740eaf705a62551748
lizq18 <li-zq18@mails.tsinghua.edu.cn>
2022-08-11 19:49:00 +0800

    Initial Version: Static charts, with simple buttons and other components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import * as echarts from 'echarts';
+import { Button } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+// import Icon from '@mui/material';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import StopIcon from '@mui/icons-material/Stop';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import FiberSmartRecordIcon from '@mui/icons-material/FiberSmartRecord';
+import React from 'react';
+
+class ForceChart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    // Temporary Option
+    let option = {
+      xAxis: {
+        type: 'category',
+        data: this.props.data.x
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: this.props.data.y,
+          type: 'line',
+          color: this.props.color
+        }
+      ]
+    };
+
+    let chart = echarts.init(document.getElementById(this.props.chartId));
+    chart.setOption(option);
+  }
+  render() {
+    return (
+      <div id={this.props.chartId} style={{width: 700, height: 300}}></div>
+    );
+  }
+}
 
 function App() {
   return (
@@ -18,6 +63,37 @@ function App() {
           Learn React
         </a>
       </header>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h5" component="h5">Cutting Force Monitor</Typography>
+        </Toolbar>
+      </AppBar>
+      <Button variant="contained" color="success" size="large">
+        Start
+        <PlayArrowIcon></PlayArrowIcon>  
+      </Button>
+      <Button variant="contained" color="error" size="large">
+        Stop
+        <StopIcon></StopIcon>
+      </Button>
+      <Button variant="contained" color="warning" size="large">
+        Record
+        <FiberManualRecordIcon></FiberManualRecordIcon>
+      </Button>
+      <Stack horizon="row" spacing={2}>
+        <ForceChart chartId="Fc" data={{
+          x: [1, 2, 3, 4, 5, 6, 7],
+          y: [150, 230, 224, 218, 135, 147, 260]
+        }} color="#ff0000"></ForceChart>
+        <ForceChart chartId="Ff" data={{
+          x: [1, 2, 3, 4, 5, 6, 7],
+          y: [150, 230, 224, 218, 135, 147, 260]
+        }} color="#0000ff"></ForceChart>
+        <ForceChart chartId="Fp" data={{
+          x: [1, 2, 3, 4, 5, 6, 7],
+          y: [150, 230, 224, 218, 135, 147, 260]
+        }} color="#00ff00"></ForceChart>
+      </Stack>
     </div>
   );
 }
